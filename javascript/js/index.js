39521b4cd8ab1bda3394ddfa4f2587f286e33b4f
denisemacharia <149757772+denisemacharia@users.noreply.github.com>
2023-11-03 08:58:35 +0300

    Add files via upload

---
+++ javascript/js/index.js
@@ -0,0 +1,7 @@
+function display(){
+//alert("Welcome to Our System!");
+document.getElementById("heading").innerHTML="This is Javascript";
+}
+function hide(){
+document.getElementById("heading").innerHTML="";
+}
