ea136e46840e2f8c247c923ab50175ca5bff7628
denisemacharia <149757772+denisemacharia@users.noreply.github.com>
2023-11-03 10:59:12 +0300

    Add files via upload

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -5,3 +5,11 @@ document.getElementById("heading").innerHTML="This is Javascript";
 function hide(){
 document.getElementById("heading").innerHTML="";
 }
+
+function read(){
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+
+    document.getElementById("fnamevalue").innerHTML=fname;
+    document.getElementById("lnamevalue").innerHTML=lname;
+}
